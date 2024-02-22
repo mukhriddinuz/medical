ad1ec473f6dd5b46c3dab66096d88ee7d7522c81
maxmorko <humoyunyolbarsov28@gmail.com>
2024-02-22 18:02:23 +0500

    humoy

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,10 @@ from .views import *
 
 
 urlpatterns = [
-    path('employee-by-position/', employee_by_position)
+    path('employee-by-position/', employee_by_position),
+    path('employee-by-gender/', employee_by_gender),
+    path('employee-by-address/', employee_by_address),
+    path('employee-by-room_id/', employee_by_room_id),
+    path('employee-by-salary/', employee_by_salary),
+    path('department_by_patient/', department_by_patient),
 ]
--- main/views.py
+++ main/views.py
@@ -12,3 +12,67 @@ def employee_by_position(request):
     ser = EmployeeSerializer(employee, many=True)
     return Response(ser.data)
 
+
+@api_view(["GET"])
+def employee_by_gender(request):
+    gender = request.GET.get('gender')
+    employee = Employee.objects.filter(gender=gender)
+    ser = EmployeeSerializer(employee, many=True)
+    return Response(ser.data)
+
+
+@api_view(["GET"])
+def employee_by_address(request):
+    address = request.GET.get('address')
+    employee = Employee.objects.filter(address=address)
+    ser = EmployeeSerializer(employee, many=True)
+    return Response(ser.data)
+
+
+@api_view(["GET"])
+def employee_by_room_id(request):
+    room_id = request.GET.get('room_id')
+    employee = Employee.objects.filter(room_id=room_id)
+    ser = EmployeeSerializer(employee, many=True)
+    return Response(ser.data)
+
+
+@api_view(["GET"])
+def employee_by_salary(request):
+    salary = request.GET.get('salary')
+    employee = Employee.objects.filter(salary=salary)
+    ser = EmployeeSerializer(employee, many=True)
+    return Response(ser.data)
+
+
+@api_view(["GET"])
+def employee_by_speciality_id(request):
+    speciality_id = request.GET.get('speciality_id')
+    employee = Employee.objects.filter(speciality_id=speciality_id)
+    ser = EmployeeSerializer(employee, many=True)
+    return Response(ser.data)
+
+
+@api_view(["GET"])
+def department_by_patient(request):
+    department = request.GET.get('department')
+    patient = PatientIllness.objects.filter(department=department)
+    ser = PatientIllnessSerializer(patient, many=True)
+    return Response(ser.data)
+
+
+@api_view(["GET"])
+def room_by_status(request):
+    status = request.GET.get('status')
+    room = Room.objects.filter(status=status)
+    ser = RoomSerializer(patient, many=True)
+    return Response(ser.data)
+
+
+
+
+
+
+
+
+
