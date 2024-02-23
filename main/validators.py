c1557c68730259f350439cb23eca42156fc23161
mukhriddinuz <109203312+Muhriddinuz@users.noreply.github.com>
2024-02-23 15:53:50 +0500

    medical

--- main/validators.py
+++ main/validators.py
@@ -8,7 +8,7 @@ class ImageFileValidator (FileExtensionValidator):
         self.allowed_extensions = allowed_extensions or ['jpg', 'jpeg', 'png', 'gif', 'bmp']
         self.message = message or 'File must be an image with a valid extension.'
 
-    def __call__(self, value) :
+    def __call__(self, value):
         # Get the file extension using a regular expression
         match = re.search(r'\. ([a-zA-Z0-9]+)$', str(value))
         if not match or match.group (1). lower() not in self.allowed_extensions:
